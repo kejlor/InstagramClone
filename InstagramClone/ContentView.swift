8332bf168953ebd23152ca94f85ab8e2077ac50b
kejlor <woj450@gmail.com>
2021-12-08 15:16:37 +0100

    First commit

--- InstagramClone/ContentView.swift
+++ InstagramClone/ContentView.swift
@@ -7,15 +7,58 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct InstagramClone: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView {
+            LazyVStack {
+                ForEach(0..<10) { index in
+                    VStack(spacing: 0) {
+                        HStack {
+                            Image("ben")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 30, height: 30)
+                                .clipShape(Circle())
+                            
+                            Text("best_left_winger")
+                                .font(.caption)
+                                .bold()
+                            
+                            Spacer()
+                            
+                            Image(systemName: "ellipsis").rotationEffect(.degrees(-90))
+                        }
+                        .padding(.horizontal)
+                        .padding(.bottom, 5)
+                        
+                        GeometryReader { geo in
+                            Image("ben")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: geo.size.width, height: geo.size.height * 1)
+                        }
+                        .frame(height: 500)
+                        
+                        HStack {
+                            Image(systemName: "heart")
+                            Image(systemName: "message")
+                            Image(systemName: "paperplane")
+                            Spacer()
+                            Image(systemName: "bookmark")
+                        }
+                        .foregroundColor(Color.primary)
+                        .padding(.horizontal)
+                    }
+                }
+            }
+        }
+
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct InstagramClone_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        InstagramClone()
+            .preferredColorScheme(.dark)
     }
 }
