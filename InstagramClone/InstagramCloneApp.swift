5ba09dbd76ead51744167307d5a6661b36adf1af
kejlor <woj450@gmail.com>
2021-12-08 15:15:26 +0100

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Bartosz Wojtkowiak on 08/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
